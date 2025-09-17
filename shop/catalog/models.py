2ddc322f30ae8beeff292f75049529dc8e7990fa
DeliciousM1lk <vladdeev00@gmail.com>
2025-09-17 20:01:11 +0500

    1

--- shop/catalog/models.py
+++ shop/catalog/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+def product_image_path(instance, filename):
+    return f"products/{instance.id}/{filename}"
+
+
 class Product(models.Model):
     name = models.CharField(verbose_name="Название", max_length=200)
+    slug = models.SlugField(verbose_name="Слаг", max_length=200, unique=True)
     price = models.DecimalField(verbose_name="Цена", max_digits=10, decimal_places=2)
     description = models.TextField(verbose_name="Описание", blank=True)
     created_at = models.DateTimeField(verbose_name="Создан", auto_now_add=True)
@@ -16,3 +21,19 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(verbose_name='Изображение', upload_to=product_image_path)
+    is_main = models.BooleanField(verbose_name='Главное изображение', default=False)
+    alt = models.CharField(verbose_name='Альтернативный текст', max_length=200, blank=True)
+    created_at = models.DateTimeField(verbose_name='Загружено', auto_now_add=True)
+
+    class Meta:
+        ordering = ['product']
+        verbose_name = "Изображение продукта"
+        verbose_name_plural = "Изображения продуктов"
+
+    def __str__(self):
+        return f'{self.pk} - {self.product.name}'
