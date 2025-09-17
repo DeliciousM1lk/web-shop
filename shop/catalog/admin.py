53dd43af218fa86f4419b260ffca726db6b3d2ec
DeliciousM1lk <vladdeev00@gmail.com>
2025-09-17 19:50:14 +0500

    1

--- shop/catalog/admin.py
+++ shop/catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import *
+
+
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description', 'created_at', 'updated_at')
+    list_display_links = ('id','name')
+    search_fields = ('name', 'description')
+    list_per_page = 10
+
+
--- shop/catalog/models.py
+++ shop/catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(verbose_name="Название", max_length=200)
+    price = models.DecimalField(verbose_name="Цена", max_digits=10, decimal_places=2)
+    description = models.TextField(verbose_name="Описание", blank=True)
+    created_at = models.DateTimeField(verbose_name="Создан", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Обновлен", auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.name
